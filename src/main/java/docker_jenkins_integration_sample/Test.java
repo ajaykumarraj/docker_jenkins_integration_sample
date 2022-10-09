95ebd4f6ba60648052705f83728ca47d1bff3991
ajaykumarraj <ajaysanjuraj@gmail.com>
2022-10-09 09:00:31 +0530

    This is my fourth commit

--- src/main/java/docker_jenkins_integration_sample/Test.java
+++ src/main/java/docker_jenkins_integration_sample/Test.java
@@ -3,8 +3,7 @@ package docker_jenkins_integration_sample;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Welcome to the new world ");
+		System.out.println("I am going to create docker image ");
 	}
 
 }
